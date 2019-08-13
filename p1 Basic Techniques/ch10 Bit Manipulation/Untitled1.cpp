c5106ced63f00556e2d6c563dd46a41f933c98dc
poojasinghal992738 <pooja.singhal992738@gmail.com>
2019-08-13 08:15:41 +0530

    Bit Manipulation started

---
+++ p1 Basic Techniques/ch10 Bit Manipulation/Untitled1.cpp
@@ -0,0 +1,17 @@
+// Bit Manipulation
+/*
+Note: This will a an intruging chapter, if you have not experience before, refer book before codes
+
+
+There are many applications & also Bit level are fatser & less complex in computers
+signed -x representation is is unsigned (2**n - x)
+There is wrap around of numbers due to overflow
+
+
+*/
+
+#include<bits/stdc++.h>
+int main()
+{
+	return 0;
+}
