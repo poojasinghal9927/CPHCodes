f0deb4cea8053eea442898a01181f3205b546d88
poojasinghal992738 <pooja.singhal992738@gmail.com>
2019-08-15 23:38:44 +0530

    updated

--- p1 Basic Techniques/ch10 Bit Manipulation/Untitled1.cpp
+++ p1 Basic Techniques/ch10 Bit Manipulation/Untitled1.cpp
@@ -2,16 +2,13 @@
 /*
 Note: This will a an intruging chapter, if you have not experience before, refer book before codes
 
-
-There are many applications & also Bit level are fatser & less complex in computers
+There are many applications & also Bit level are faster & less complex in computers
 signed -x representation is is unsigned (2**n - x)
-There is wrap around of numbers due to overflow
-
-
+There is wrap around of numbers due to overflow, which comes in both signed and unsigned
 */
 
 #include<bits/stdc++.h>
-int main()
+main()
 {
 	return 0;
 }
