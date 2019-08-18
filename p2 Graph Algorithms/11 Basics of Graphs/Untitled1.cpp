a7d6fc094c50f7f123391092518ae4c475333346
poojasinghal992738 <pooja.singhal992738@gmail.com>
2019-08-18 23:41:05 +0530

    BEGIN GRAPH ALGORITHMS

---
+++ p2 Graph Algorithms/11 Basics of Graphs/Untitled1.cpp
@@ -0,0 +1,25 @@
+// Basics of Graphs
+/*
+n be number of nodes/vertices
+m be number of edges
+
+"path" from a to b, is sequence of edges form a to b, through graph edges
+"length" of path is number of edges in it
+"cycle" if first & last node is same
+"simple" path in which each node appears only once
+
+Connectivity
+
+"connected" is path between any two nodes
+"component", is each connected part of graph
+"tree" is connected graph, with unique path between any two nodes & n-1 edges
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
