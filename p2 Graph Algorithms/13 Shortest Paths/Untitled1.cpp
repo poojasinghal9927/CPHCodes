03e34cc69322d087d73041d23ff66be5dec0be18
poojasinghal992738 <pooja.singhal992738@gmail.com>
2019-08-21 16:20:18 +0530

    shortest paths

--- p2 Graph Algorithms/13 Shortest Paths/Untitled1.cpp
+++ p2 Graph Algorithms/13 Shortest Paths/Untitled1.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 void bellman_ford(int x)
 {
+	// Edge List representation assumed
 	for (int i = 0; i < n; i++)
 		distance[i] = INF;
 	distance[x] = 0;
@@ -21,6 +22,39 @@ void bellman_ford(int x)
 			int a, b, w;
 			tie(a, b, w) = e;
 			distance[b] = min(distance[b], distance[a]+w);
+		}
+	}
+	/*
+	Note: an improvement to bellmann ford (SPFA), you can use a queue
+	to place target of the relax edges in that queue, next time	edges
+	starting from those target vertices should be tried for	relaxation,
+	not all vertices.
+	Worst case will still be O(m*n)
+	*/
+}
+
+void dijkstra(int x)
+{
+	queue<pair<int,int>> q;
+	// Adjacency list representation a -> [(b,w),] assumed
+	for (int i = 0; i < n; i++)
+		distance[i] = INF;
+	distance[x] = 0;
+	q.push(make_pair(0,x));
+	while (!q.empty())
+	{
+		int a = q.top().second; q.pop();
+		if (processed[a])
+			continue;
+		processed[a] = true;
+		for (auto u : adj[a])
+		{
+			int b = u.first, w = u.second;
+			if (distance[a]+w < distance[b])
+			{
+				distance[b] = distance[a]+w;
+				q.push({-distance[b],b});
+			}
 		}
 	}
 }
